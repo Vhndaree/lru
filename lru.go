01cc099cf1cb2f4d3603d30a1580452aae6a76dc
Ram Bhandari <ramkumar.bhandari@rewe-digital.com>
2023-08-16 17:11:34 +0200

    org. folder

--- lru.go
+++ lru.go
@@ -2,6 +2,27 @@ package lru
 
 import "sync"
 
+// LRU is a generic interface representing a Least Recently Used (LRU) cache.
+type LRU[K comparable, V any] interface {
+	// ListAll returns a map containing all key-value pairs in the LRU cache.
+	ListAll() map[K]V
+
+	// Contains checks if a key is present in the LRU cache.
+	Contains(key K) bool
+
+	// Set adds or updates a key-value pair in the LRU cache.
+	Set(key K, value V)
+
+	// Get retrieves the value associated with the given key from the LRU cache.
+	// If the key is found, the associated value and true are returned; otherwise,
+	// a default value and false are returned.
+	Get(key K) (value V, found bool)
+
+	// Del removes a key-value pair from the LRU cache and returns true if successful.
+	// If the key is not found, it returns false.
+	Del(key K) bool
+}
+
 // cache is a generic struct representing an item in a cache.
 type cache[K comparable, V any] struct {
 	key   K            // The key associated with the cache item.
@@ -22,7 +43,7 @@ type lru[K comparable, V any] struct {
 
 // New creates a new instance of a Least Recently Used (LRU) cache with the specified size.
 // It returns a pointer to an lru[K, V] instance.
-func New[K comparable, V any](size int) *lru[K, V] {
+func New[K comparable, V any](size int) LRU[K, V] {
 	return &lru[K, V]{
 		cache:  map[K]*cache[K, V]{},
 		size:   size,
